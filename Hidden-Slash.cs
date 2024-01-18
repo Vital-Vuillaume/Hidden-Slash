b8dd8f5b967d282f0053828e2947011152b5d552
Vital Vuillaume <vitalvuillaume@proton.me>
2024-01-18 20:04:30 +0100

    Update Hidden-Slash.cs

--- Hidden-Slash.cs
+++ Hidden-Slash.cs
@@ -1,43 +1,92 @@
+
 using System;
 
-namespace MyCompiler {
-    class Program {
-        public static void Main(string[] args) {
-            
-            // Random nb
-            Random rng = new Random();
-            int nbRng = rng.Next(0, 101);
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] letters = { "a", "c", "s", "m", "k", "b", "p" };
 
-            // Input validity
-            int inputResult;
-            bool isValidInput = false;
-            
-            // Sentence
-            string sentence = "Entrez un nombre entre 1 et 100 : ";
-            Console.WriteLine(sentence);
-            
-            do
-            {
-                string input = Console.ReadLine();
-                isValidInput = int.TryParse(input, out inputResult);
-                
-                if (!isValidInput)
-                {
-                    Console.WriteLine(sentence);
-                }
-                else if (inputResult > nbRng)
-                {
-                    Console.WriteLine("Le nombre " + inputResult + " est trop grand.");
-                }
-                else if (inputResult < nbRng)
-                {
-                    Console.WriteLine("Le nombre " + inputResult + " est trop petit.");
-                }
-                
-            } while (!isValidInput || inputResult != nbRng);
+        Random rng = new Random();
 
-            // Result
-            Console.WriteLine("Bien joue c'etait le nombre " + nbRng + ".");
+        int rngIndex = rng.Next(0, letters.Length);
+        int slashIndex = rng.Next(0, 305);
+
+        Console.WriteLine("Find the letter and write it down:\n");
+
+        string stringWithSlashes = $"/////////////////////////////////\n/////////////////////////////////\n/////////////////////////////////\n/////////////////////////////////\n/////////////////////////////////\n/////////////////////////////////\n/////////////////////////////////\n/////////////////////////////////\n/////////////////////////////////";
+
+        // Convertir la chaîne en tableau de caractères pour pouvoir modifier les éléments individuellement
+        char[] charArray = stringWithSlashes.ToCharArray();
+
+        // Remplacer le slash à l'index aléatoire par une lettre aléatoire
+        charArray[slashIndex] = Convert.ToChar(letters[rngIndex]);
+
+        Console.WriteLine(new string(charArray));
+
+        Console.WriteLine("\nWrite it down at the bottom:");
+        string input = Console.ReadLine();
+
+        while (input != letters[rngIndex])
+        {
+            Console.WriteLine("You're wrong");
+            Console.WriteLine("Try again:");
+            input = Console.ReadLine();
+        }
+
+        switch (input)
+        {
+            case "a":
+                Console.WriteLine("   \\\n  (o>\n\\\\_//)\n \\_/_)");
+                break;
+            case "c":
+                Console.WriteLine("\n / \\__\n(    @\\___\n /         O\n/   (_____/\n/_____/   U");
+                break;
+            case "s":
+                Console.WriteLine(@"
+                Singe
+                
+                            ,.-"" ""-.,
+                           /   ===   \
+                          /  =======  \
+                       __|  (o)   (0)  |__      
+                      / _|    .---.    |_ \         
+                     | /.----/ O O \----.\ |       
+                      \/     |     |     \/        
+                      |                   |            
+                      |                   |           
+                      |                   |          
+                      _\   -.,_____,.-   /_         
+                  ,.-""  ""-.,_________,.-""  ""-.,
+                 /          |       |          \  
+                |           l.     .l           | 
+                |            |     |            |
+                l.           |     |           .l             
+                 |           l.   .l           | \,     
+                 l.           |   |           .l   \,    
+                  |           |   |           |      \,  
+                  l.          |   |          .l        |
+                   |          |   |          |         |
+                   |          |---|          |         |
+                   |          |   |          |         |
+                   /""-.,__,.-""\   /""-.,__,.-""\""-,_,.-""\
+                  |            \ /            |         |
+                  |             |             |         |
+                   \__|__|__|__/ \__|__|__|__/ \_|__|__/
+                ");
+                break;
+            case "m":
+                Console.WriteLine("   /\\_/\\ \n   (o o) \n   (  \"  ) \n   \\~*~/ \n   /   \\");
+                break;
+            case "k":
+                Console.WriteLine("     \\ \n     (\\_ \n     (-\\ \n     / \\) \n   __\\_\\__ \n  (       ) \n   \\  |  / \n    \\ | / \n     \\|/");
+                break;
+            case "b":
+                Console.WriteLine("   (\n  `-'-.\n  '( @ >\n   _) (\n  /    )\n /_,'  /\n   \\  /\n===m\"\"m===");
+                break;
+            case "p":
+                Console.WriteLine("     /\\ \n    (o>\\ \n   //\\  \\> \n   V  \\ ) \n        |");
+                break;
         }
     }
 }
